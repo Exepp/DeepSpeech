c8b77f1f960abdc2c3bdcdd082eb8aea62e2689d
Paweł Głomski <pavel.glomski@gmail.com>
2021-06-13 03:16:45 +0200

    Fix anaconda install

--- zako_env.sh
+++ zako_env.sh
@@ -2,8 +2,10 @@
 if ! command -v conda &> /dev/null
 then
     echo "Downloading conda"
-    wget "https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh"
-    ./Anaconda3-2021.05-Linux-x86_64.sh
+    wget "https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh" && \
+        chmod +x ./Anaconda3-2021.05-Linux-x86_64.sh && \
+        ./Anaconda3-2021.05-Linux-x86_64.sh
+        source ~/.bashrc
 fi
 eval "$(conda shell.bash hook)"
 
